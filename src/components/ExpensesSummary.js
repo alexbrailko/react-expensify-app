cffd57063ba1a5fc2bc5e0ae8d9f22717564f01e
Aleksey Brailko <alexbrailko@gmail.com>
2017-11-15 19:19:57 +0200

    added Not Showing expenses in ExpensesSummary component

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,15 +6,24 @@ import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+export const ExpensesSummary = ({ expenseCount, expensesTotal, notShowingExpenseCount }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const notShowingexpenseWord = notShowingExpenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+
     return (
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page-header__title">
                     Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span>
                 </h1>
+                
+                { notShowingExpenseCount > 0 &&  
+                    <span className="page-header__hidden_expenses">
+                        Not showing <span>{notShowingExpenseCount}</span> {notShowingexpenseWord} because of filters.
+                    </span>
+                }
+
                 <div className="page-header__actions">
                     <Link className="button" to="/create">Add Expense</Link>
                 </div>
@@ -25,10 +34,12 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
+    const notShowingExpenseCount = state.expenses.length - visibleExpenses.length;
 
     return {
         expenseCount: visibleExpenses.length,
-        expensesTotal: selectExpensesTotal(visibleExpenses)
+        expensesTotal: selectExpensesTotal(visibleExpenses),
+        notShowingExpenseCount: notShowingExpenseCount
     };
 };
   
